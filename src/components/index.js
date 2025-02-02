dc507c2b2120b143eb59b5ce7a532633dd09ffe4
Cristian Guerrero <1cristiang@gmail.com>
2025-02-01 23:22:28 -0300

    subiendo componente RegisterForm y Page Register

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 export * from './componentExample';
 export { default as LoginModal } from './LoginModal/LoginModal';
 export * from './ui';
+export { default as RegisterForm } from './registerform/RegisterForm';
