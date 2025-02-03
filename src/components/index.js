b3fd8941fb534393640f0ea375cf2e0615305834
alonso trina <alonsotrina22@gmail.com>
2025-02-03 10:58:22 -0300

    actualziación del index.js folder components

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,6 @@
 export * from './componentExample';
 export { default as LoginModal } from './LoginModal/LoginModal';
+export { default as CaregoryCard } from './CategoryCard/CaregoryCard';
+export { default as Footer } from './Footer/Footer';
+export { default as HeaderHome } from './HeaderHome/HeaderHome';
 export * from './ui';
