a56cd78ace8135e44bad1fa293249068820bd848
alonso trina <alonsotrina22@gmail.com>
2025-02-03 10:59:19 -0300

    subiendo archivos utils data

---
+++ src/utils/constants/products.js
@@ -0,0 +1,84 @@
+import ProductImage from "../../assets/products.jpg";
+
+export const products = [
+  { 
+    id: '1', 
+    name: 'Tarta de Chocolate Supremo', 
+    price: 10999, 
+    category: 'Sin gluten', 
+    href: '/basket', 
+    src: ProductImage 
+  },
+  { 
+    id: '2', 
+    name: 'Pie de Limón Cremoso', 
+    price: 12999, 
+    category: 'Pie', 
+    href: '/basket', 
+    src: ProductImage  
+  },
+  { 
+    id: '3', 
+    name: 'Kuchen de Frambuesa Artesanal', 
+    price: 13999, 
+    category: 'Kuchen', 
+    href: '/basket', 
+    src: ProductImage  
+  },
+  { 
+    id: '4', 
+    name: 'Torta Tres Leches', 
+    price: 14999, 
+    category: 'Tortas', 
+    href: '/basket', 
+    src: ProductImage  
+  },
+  { 
+    id: '5', 
+    name: 'Brownie Sin Gluten', 
+    price: 8999, 
+    category: 'Sin gluten', 
+    href: '/basket', 
+    src: ProductImage 
+  },
+  { 
+    id: '6', 
+    name: 'Pie de Manzana Clásico', 
+    price: 11999, 
+    category: 'Pie', 
+    href: '/basket', 
+    src: ProductImage  
+  },
+  { 
+    id: '7', 
+    name: 'Kuchen de Nuez y Caramelo', 
+    price: 15999, 
+    category: 'Kuchen', 
+    href: '/basket', 
+    src: ProductImage  
+  },
+  { 
+    id: '8', 
+    name: 'Torta de Zanahoria Esponjosa', 
+    price: 13999, 
+    category: 'Tortas', 
+    href: '/basket', 
+    src: ProductImage  
+  },
+  { 
+    id: '9', 
+    name: 'Galletas de Avena Sin Gluten', 
+    price: 7999, 
+    category: 'Sin gluten', 
+    href: '/basket', 
+    src: ProductImage 
+  },
+  { 
+    id: '10', 
+    name: 'Pie de Queso Frutal', 
+    price: 13499, 
+    category: 'Pie', 
+    href: '/basket', 
+    src: ProductImage  
+  }
+];
