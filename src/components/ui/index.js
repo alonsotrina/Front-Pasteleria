92bcb779245171942bd4f11e155f29ab0696923d
alonso trina <alonsotrina22@gmail.com>
2025-02-03 10:57:34 -0300

    creación del components UI

--- src/components/ui/index.js
+++ src/components/ui/index.js
@@ -1,1 +1,4 @@
 export { default as ModalComponent } from './ModalComponent';
+// export { default as DropdownMenu } from './DropdownMenu';
+export { default as CustomButtom } from './CustomButton';
+export { default as CardProduct } from './CardProduct';
