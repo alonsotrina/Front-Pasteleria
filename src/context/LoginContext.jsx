baa36afb238b2c897ef01ddea9933c6140e2331b
alonso trina <alonsotrina22@gmail.com>
2025-02-06 07:05:53 -0300

    creación de conexión del back del login

--- src/context/LoginContext.jsx
+++ src/context/LoginContext.jsx
@@ -4,6 +4,9 @@ import useIsOpen from "../hooks/useIsOpen";
 
 export const LoginContext = createContext(null);
 
+const urlBase = "http://localhost:5100/api";
+const initialStateToken = localStorage.getItem("token") || null;
+
 const initialState = {
     id: null,
     email: 'alonsotrina@gmail.com',
@@ -15,31 +18,78 @@ const initialState = {
 
 const LoginProvider = ({ children }) => {
     const { state, toggle:openModal } = useIsOpen()
-    const { handleSetStorageSession, handleGetStorageSession, decrypted } = useStorage();
+    const { handleSetStorageSession, handleGetStorageSession, handleRemoveStorageSession, decrypted } = useStorage();
     const [session, setSession] = useState(initialState);
     const [isLoading, setIsLoading] = useState(false);
 
-    const handleSession = (data) => {
-        setSession(data);
-        handleSetStorageSession(data);
+    const handleSession = async (email, password) => {
+        try {
+            const response = await fetch(`${urlBase}/auth/login`, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({ email, password }),
+            });
+
+            if (!response.ok) {
+                const errorData = await response.json();
+                setSession({
+                    msg: errorData.error,
+                    showMsg: true
+                })
+                return
+            }
+
+            const data = await response.json();
+
+            setSession({
+                token: data.token,
+                msg: 'Registro exitoso',
+                showMsg: false
+            })
+
+            handleSetStorageSession(data)
+
+            // Cerrar modal inicar sesión 
+            openModal('modalOpen')
+
+            return data;
+        }
+        catch (error) {
+            setSession({
+                email: '',
+                token: initialStateToken,
+                msg: `Error: ${error.message || "Problema de conexión"}`,
+                showMsg: true
+            })
+        }
     };
+
 
     useEffect(() => {
         handleGetStorageSession();
     }, []);
 
-    // useEffect(() => {
-    //     if (decrypted) {
-    //         console.log('Decrypted value:', decrypted); 
-    //         setSession(JSON.parse(decrypted)); 
-    //     } else {
-    //         console.log('No se encontró sesión en el almacenamiento');
-    //     }
-    //     setIsLoading(false);
-    // }, [decrypted]);
+    useEffect(() => {
+        if (decrypted) {
+            console.log('Decrypted value:', decrypted); 
+            setSession(JSON.parse(decrypted)); 
+        } else {
+            console.log('No se encontró sesión en el almacenamiento');
+        }
+        setIsLoading(false);
+    }, [decrypted]);
+
+    handleRemoveStorageSession
+
+    const logout = () => {
+        setSession(initialState)
+        handleRemoveStorageSession()
+    };
 
     return (
-        <LoginContext.Provider value={{ state, openModal, session, isLoading, handleSession }}>
+        <LoginContext.Provider value={{ state, openModal, session, isLoading, handleSession,logout }}>
             {children}
         </LoginContext.Provider>
     );
--- src/hooks/useAuth.jsx
+++ src/hooks/useAuth.jsx
@@ -2,13 +2,14 @@ import { useContext } from 'react';
 import { LoginContext } from '../context/LoginContext';
 
 export const useAuth = () => {
-  const { state, openModal, session, isLoading, handleSession } = useContext(LoginContext) 
+  const { state, openModal, session, isLoading, handleSession, logout } = useContext(LoginContext) 
 
   return { 
     state, 
     openModal,
     session, 
     isLoading, 
-    handleSession 
+    handleSession,
+    logout
   };
 }
