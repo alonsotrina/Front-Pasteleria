156793d1b6c2ffe7607124d884c39d6e21d5e685
alonso trina <alonsotrina22@gmail.com>
2025-02-14 06:29:34 -0300

    creación componenetes para lista de productos

--- src/main.jsx
+++ src/main.jsx
@@ -4,16 +4,22 @@ import App from './App.jsx'
 import { BrowserRouter as Router } from "react-router-dom";
 import LoginProvider from "./context/LoginContext.jsx";
 import BasketProvider from "./context/BasketContext.jsx";
+import ComunasProvider from './context/ComunasContext.jsx';
+import ProductProvider from './context/ProductContext.jsx';
 import '../src/assets/tailwind.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-     <LoginProvider>
-      <BasketProvider>
-        <Router>
-          <App />
-        </Router>
-      </BasketProvider>
-     </LoginProvider>
+    <ComunasProvider>
+      <LoginProvider>
+        <ProductProvider>
+          <BasketProvider>
+            <Router>
+              <App />
+            </Router>
+          </BasketProvider>
+        </ProductProvider>
+      </LoginProvider>
+    </ComunasProvider>
   </React.StrictMode>,
 )
